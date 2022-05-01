4a623ce9d81eaa9e78fc7ebba68a283a33a46676
sf <yixinhai@test.com>
2022-05-01 14:08:48 +0800

    hot-fix

--- src/main/java/com/test/vs/GitTest.java
+++ src/main/java/com/test/vs/GitTest.java
@@ -4,5 +4,8 @@ public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("version: " + "hot_fix");
+        System.out.println("version: " + "hot_fix");
+        System.out.println("version: " + "hot_fix");
+        System.out.println("version: " + "hot_fix");
     }
 }
