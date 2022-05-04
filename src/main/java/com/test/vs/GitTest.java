b6f678fa7a53b4bf9b4926c4a9e688c5c366363a
sf <yixinhai@test.com>
2022-05-04 10:46:54 +0800

    github_2.1

--- src/main/java/com/test/vs/GitTest.java
+++ src/main/java/com/test/vs/GitTest.java
@@ -3,14 +3,8 @@ package com.test.vs;
 public class GitTest {
 
     public static void main(String[] args) {
-        System.out.println("version: " + "hot_fix");
-        System.out.println("version: " + "hot_fix");
-        System.out.println("version: " + "hot_fix");
-        System.out.println("version: " + "hot_fix");
-        System.out.println("version: " + "hot_fix");
-        System.out.println("version: " + "hot_fix");
-        System.out.println("version: " + "hot_fix");
-        System.out.println("version: " + "hot_fix");
-        System.out.println("version: " + "hot_fix");
+        System.out.println("hot_fix: 1.0");
+        System.out.println("hot_fix: 2.0");
+        System.out.println("hot_fix: 3.0");
     }
 }
